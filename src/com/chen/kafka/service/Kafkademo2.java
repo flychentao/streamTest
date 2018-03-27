dc5bfa12b8cc983087ec418cb04d58e51b5987c7
chentao <1148781827@qq.com>
2018-03-27 18:52:03 +0800

    kafka stream test

---
+++ src/com/chen/kafka/service/Kafkademo2.java
@@ -0,0 +1,28 @@
+package com.chen.kafka.service;
+
+import java.util.Properties;
+
+import org.apache.kafka.streams.KafkaStreams;
+import org.apache.kafka.streams.StreamsBuilder;
+import org.apache.kafka.streams.StreamsConfig;
+import org.apache.kafka.streams.Topology;
+
+public class Kafkademo2 {
+
+	public static void main(String[] args) {
+		Properties props = new Properties();
+		props.put("application.id", "");
+		props.put("bootstrap.servers", "");
+		
+		StreamsBuilder builder = new StreamsBuilder();
+		StreamsConfig config = new StreamsConfig(props);
+		Topology topology = builder.build();
+		KafkaStreams streams = new KafkaStreams(topology, config);
+		streams.setUncaughtExceptionHandler((Thread thread, Throwable throwable) -> {
+			  // here you should examine the throwable/exception and perform an appropriate action!
+			});
+		streams.start();
+		Runtime.getRuntime().addShutdownHook(new Thread(streams::close));
+
+	}
+}
